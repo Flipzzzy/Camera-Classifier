0c204d0aa3c0adf49940b05f21951f7d4fc5a981
KulikovMPT <isip_k.s.kulikov@mpt.ru>
2024-01-10 15:53:00 +0300

    Camera Classifier beta v1

---
+++ camera.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+
+class Camera:
+    def __init__(self) -> None:
+        self.camera = cv.VideoCapture(0)
+        if not self.camera.isOpened():
+            raise ValueError("Не удалось включить камеру")
+        
+        self.width = self.camera.get(cv.CAP_PROP_FRAME_WIDTH)
+        self.height = self.camera.get(cv.CAP_PROP_FRAME_HEIGHT)
+        
+    def __del__(self):
+        if self.camera.isOpened():
+            self.camera.release()
+            
+    def get_frame(self):
+        if self.camera.isOpened():
+            ret, frame = self.camera.read()
+            
+            if ret:
+                return (ret, cv.cvtColor(frame, cv.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return None
---
+++ main.py
@@ -0,0 +1,7 @@
+import app
+
+def main():
+    app.App(window_title="Camera Classifire")
+    
+if __name__ == '__main__':
+    main()
